add4292894cb7bc592ef82e369ee483c6c41c34b
Daniel Grund <istaranmajin@gmail.com>
2019-04-14 00:58:08 -0700

    New game scene with male/female choice. Old saves invalid. Stamina costing. Increased nutrition values. Chain verb and scene engine up and running.

--- sceneengine.js
+++ sceneengine.js
@@ -1,8 +1,20 @@
 var cache = require('./cache');
+var gameengine = null;
 
-let getControls = async function (state) {
+let getControls = async function (state, scene) {
+    if (!gameengine) gameengine = require('./gameengine');
+
+    let sceneDef = await cache.load(`data/scenes/${scene.name}.json`);
+    let subscene = sceneDef[scene.sub];
     let controls = [];
-    // Add options based on scene unfolding.
+    for (var i = 0; i < subscene.controls.length; i++) {
+        controls.push([]);
+        for (var j = 0; j < subscene.controls[i].length; j++) {
+            let ctrl = await gameengine.getControl(state, subscene.controls[i][j]);
+            if (ctrl)
+                controls[i].push(ctrl);
+        }
+    }
     return controls;
 }
 
