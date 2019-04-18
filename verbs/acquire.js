f6935b531235fce0797947ad03c018c4d6b1ae7c
DanielGrund <45804877+DanielGrund@users.noreply.github.com>
2019-04-18 12:01:02 -0700

    rearrange private assets and try to make the game work without them (skip google oauth without the config)

--- verbs/acquire.js
+++ verbs/acquire.js
@@ -4,7 +4,7 @@ var cache = require('../cache');
 
 let act = async function (state, details) {
     let enemyDef = await cache.load(`data/enemies/${state.enemy.name}.json`);
-    let enemyPrivateDef = (state.query.nsfw ? await cache.load(`data/private/enemies/${state.enemy.name}.json`) : null);
+    let enemyPrivateDef = (state.query.nsfw ? await cache.load(`private/enemies/${state.enemy.name}.json`) : null);
     let acquireCard = (enemyPrivateDef && enemyPrivateDef.acquirecards && enemyPrivateDef.acquirecards[details.card]) ? enemyPrivateDef.acquirecards[details.card] : enemyDef.acquirecards[details.card];
     await combatengine.clearCombat(state);
     if (acquireCard) { // Switching modes my end in a no-op maybe? Actually, status call should prevent.
