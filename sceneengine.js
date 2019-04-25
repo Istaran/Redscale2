c2a7d01d15c28f97d0866200c81a9881d4167838
DanielGrund <45804877+DanielGrund@users.noreply.github.com>
2019-04-25 14:51:47 -0700

    Kobold content, and try to use session state to preserve nsfw=true

--- sceneengine.js
+++ sceneengine.js
@@ -3,17 +3,29 @@ var gameengine = null;
 
 let getControls = async function (state, scene) {
     if (!gameengine) gameengine = require('./gameengine');
-
+    let private = state.query.nsfw == 'true' && await cache.load(`./private/scenes/${scene.name}.json`);
     let sceneDef = await cache.load(`data/scenes/${scene.name}.json`);
-    let subscene = sceneDef[scene.sub];
+    let subscene = (private && private[scene.sub]) || sceneDef[scene.sub];
     let controls = [];
-    for (var i = 0; i < subscene.controls.length; i++) {
-        controls.push([]);
-        for (var j = 0; j < subscene.controls[i].length; j++) {
-            let ctrl = await gameengine.getControl(state, subscene.controls[i][j]);
-            if (ctrl)
-                controls[i].push(ctrl);
+    if (subscene) {
+        for (var i = 0; i < subscene.controls.length; i++) {
+            controls.push([]);
+            for (var j = 0; j < subscene.controls[i].length; j++) {
+                let ctrl = await gameengine.getControl(state, subscene.controls[i][j]);
+                if (ctrl)
+                    controls[i].push(ctrl);
+            }
         }
+    } else {
+        controls.push([{
+            "type": "actButton",
+            "display": "Nevermind.",
+            "verb": "setscene",
+            "details": {
+                "text": "Whatever you were up to, you're not feeling it now.",
+                "clear": true
+            }
+        }]);
     }
     return controls;
 }
