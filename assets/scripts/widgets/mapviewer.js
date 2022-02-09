dc3308332509d8bc02c2a97c7ca5da36aa61065e
Daniel Grund <istaranmajin@gmail.com>
2022-02-08 22:14:11 -0800

    WIP map viewer update

--- assets/scripts/widgets/mapviewer.js
+++ assets/scripts/widgets/mapviewer.js
@@ -18,7 +18,8 @@ class MapViewer extends Widget {
             let location = data.current;
             let map = data;
             map.current = undefined;
-            self.setState({ map: map, location: location, you: location });
+            self.setState({
+                map: map, location: location, you: Object.assign({}, location) });
 
         }).catch(function (err) {
             setError();
@@ -30,6 +31,44 @@ class MapViewer extends Widget {
         let loc = this.state.location;
         loc.z = l;
         this.setState({ location: loc });
+    }
+
+    setZone(z) {
+        let self = this;
+        let loc = self.state.location;
+        loc.map = z;
+        let map = self.state.map[z];
+        if (map) {
+            if (z == self.state.you.map)
+                loc.z = self.state.you.z;
+            else {
+                let looped = false;
+                while (!self.anyRooms(map[loc.z]))
+                {
+                    loc.z++;
+                    if (loc.z > map.length) {
+                        loc.z = 0;
+                        if (looped) break;
+                        looped = true;
+                    }
+                }
+            }
+        }
+        self.setState({ location: loc });
+    }
+
+    anyRooms(lvl) {
+        let any = false;
+        if (lvl) {
+            lvl.forEach((row) => {
+                if (row) {
+                    row.forEach((cell) => {
+                        if (cell) any = true;
+                    });
+                }
+            });
+        }
+        return any;
     }
 
     renderInternal() {        
@@ -53,18 +92,21 @@ class MapViewer extends Widget {
                 var levelRender = <div class="map-level">{rows}</div>;
             }
             var levelMenu = <div class="map-level-menu">{map.map((lvl, lvlIdx) => {
-                if (lvl) {
-                    if (lvlIdx < self.state.you.z)
-                        return <div class="map-level-button" key={"level_" + lvlIdx} onClick={() => self.setLevel(lvlIdx)}>Down {self.state.you.z - lvlIdx}</div>;
-                    else if (lvlIdx > self.state.you.z)
-                        return <div class="map-level-button" key={"level_" + lvlIdx} onClick={() => self.setLevel(lvlIdx)}>Up {lvlIdx - self.state.you.z}</div>;
-                    return <div class="map-level-button" key={"level_" + lvlIdx} onClick={() => self.setLevel(lvlIdx)}>Current</div>;
+                if (this.anyRooms(lvl)) {
+                    if (self.state.you.map == self.state.location.map && lvlIdx == self.state.you.z)
+                        return <div class="map-level-button" key={"level_" + lvlIdx} onClick={() => self.setLevel(lvlIdx)}>Here</div>;
+                    return <div class="map-level-button" key={"level_" + lvlIdx} onClick={() => self.setLevel(lvlIdx)}>--*--</div>;
+                }
+            })}</div>;
+            var zoneMenu = <div class="map-zone-menu">{Object.keys(this.state.map).map((zone, zoneIdx) => {
+                if (self.state.map[zone]) {
+                    return <div class="map-zone-button" key={"zone_" + zoneIdx} onClick={() => self.setZone(zone)}>{zone}</div>;
                 }
             })}</div>;
             
         }
 
 
-        return <div class="map">{levelMenu}{ levelRender }</div>;
+        return <div class="map">{zoneMenu}{levelMenu}{ levelRender }</div>;
     }
 }
